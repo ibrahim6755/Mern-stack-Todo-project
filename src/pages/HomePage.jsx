0b8e658e4c8af1dc0c9f44239f628f37ce177a05
ibrahim6755 <ibrahimghani020@gmail.com>
2023-03-20 15:03:12 +0000

    changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import bg from '../imgs/overlay.jpg'
+import Styles from '../pages/HomePageStyles.css'
 
 function HomePage() {
   return (
    <>
+   <div className= {`bg ${Styles.bg}`}>
+      <img className='w-100' src={bg} alt="" />
+    </div>
    <div className="row">
-    <div className="col-md-6">
-    <h1 className='mt-5'>Welcome to Todo App</h1>
-    <p className='text-center'>If you want to create Todo First Signup and if already have a account then Login..</p>
+    <div className="col-md-12 ">
+    <h1 className='mt-5 text-light'>Welcome to Todo App</h1>
+    <h5 className='text-center text-light'>If you want to create Todo First Signup and if already have a account then Login..</h5>
     <div className="display d-flex align-items-center justify-content-center">
-    <Link to="/login"><h2 className='conatiner text-center mt-5 active'>Login</h2></Link>
-     <Link to="/signup"><h2 className='conatiner text-center mt-5 ms-5'>Signup</h2></Link>
+    <Link to="/login"><h2 className='conatiner text-center mt-5 active text-light'>Login</h2></Link>
+     <Link to="/signup"><h2 className='conatiner text-center mt-5 ps-5 text-light'>SignUp</h2></Link>
     </div>
    </div>
-   
     </div>
+   
    </>
   )
 }
